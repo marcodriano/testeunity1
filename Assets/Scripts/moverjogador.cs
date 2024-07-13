5d04a7022d1ab7b2d11298da05e4351cb71891b4
pochetevirtual <marcosadr@outlook.com>
2024-07-13 18:17:43 -0300

    pular e animações

--- Assets/Scripts/moverjogador.cs
+++ Assets/Scripts/moverjogador.cs
@@ -7,26 +7,31 @@ using UnityEngine.UIElements;
 public class moverjogador : MonoBehaviour
 {
     public float movespeed = 5f;
+    public float jumpForce = 5f;
     private Rigidbody2D rb;
     private float moveInput;
     private float position = 1;
     private Animator animator;
     private bool isRunning = false;
+    private bool isJumping = false;
+
 
     // Start is called before the first frame update
-    void Start()
+    void Start() // atualiza antes do jogo começar
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        animator.SetBool("isRunning", false);
+        rb = GetComponent<Rigidbody2D>(); // para chamar a gravidade com o nome "rb"
+        animator = GetComponent<Animator>(); // para chamar a animação com nome "animator"
+        animator.SetBool("isRunning", false); // config. para iniciar parado ou walk
+        animator.SetBool("isJumping", false); // config. para iniciar parado
+
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() // atualiza a cada frame
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
+        moveInput = Input.GetAxisRaw("Horizontal"); // Para config. o input para corrida na horizontal
 
-        if (moveInput != 0)
+        if (moveInput != 0) // if/else para saber se o input for diferente de zero, está correndo
         {
             isRunning = true;
         }
@@ -35,15 +40,20 @@ public class moverjogador : MonoBehaviour
             isRunning = false;
         }
 
-        animator.SetBool("isRunning", isRunning);
+        animator.SetBool("isRunning", isRunning); // Para setar animação Run
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping) // Função para correr se pressionar a tecla space e se não estiver pulando
+        {
+            Jump();
+        }
 
     }
-    void FixedUpdate()
+    void FixedUpdate() // atualiza a cada momento que o input acontecer
     {
 
-        rb.velocity = new Vector2(moveInput * movespeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput * movespeed, rb.velocity.y); // para calcular a velocidae da corrida
 
-        if (moveInput > 0)
+        if (moveInput > 0) // esse if/else é para decidir a direção do personagem e a animação com o input. input positivo é movimento da esquerda para direita
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
             position = 1f;
@@ -58,6 +68,25 @@ public class moverjogador : MonoBehaviour
         else
         {
             transform.localScale = new Vector3(position, 1f, 1f);
+        }
+
+        if (Mathf.Abs(rb.velocity.y) > 0.01f) // para definir quando a config da animação de pular é verdadeira (eixo y)
+        {
+            isJumping = true; 
+            animator.SetBool("isJumping", true);
+        } else 
+        {
+            isJumping = false ;
+            animator.SetBool("isJumping", false);
+        }
+    }
+    void Jump() // função para pular chamado lá em cima no update
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f); // para saber se o objeto está colidindo com o chão
+        if (hit.collider != null) // caso esteja colodindo com o chão, config um vetor com força de pulo
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce); // define a força vetorial do pulo
+            animator.SetBool("isJumping", true); // ativar a animação quando a função jump for chamada
         }
     }
 }
